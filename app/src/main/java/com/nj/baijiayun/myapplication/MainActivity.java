d7d6faf719e1f398f77ae0d931efaa9ca9c3598d
chengang <1410488687@qq.com>
2020-02-19 21:33:11 +0800

    test

--- app/src/main/java/com/nj/baijiayun/myapplication/MainActivity.java
+++ app/src/main/java/com/nj/baijiayun/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.nj.baijiayun.myapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((TextView) findViewById(R.id.tv)).setText(MessageFormat.format("{0}\n{1}\n{2}", BuildConfig.APPLICATION_ID
+                , BuildConfig.BASE_H5_URL
+                , BuildConfig.BASE_URL
+
+        ));
     }
 }
